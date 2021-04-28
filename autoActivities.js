50f3b40854234ca0d053183416f199ba9d5b9936
acedron <acedrons@yahoo.co.jp>
2021-04-28 11:36:44 +0300

    Detect minimized windows if option is true.

--- autoActivities.js
+++ autoActivities.js
@@ -37,11 +37,12 @@ var AutoActivities = GObject.registerClass(
       this._workspaceAddedEvent = global.workspace_manager.connect('workspace-added', this._onWorkspaceAdded.bind(this));
       this._workspaceRemovedEvent = global.workspace_manager.connect('workspace-removed', this._onWorkspaceRemoved.bind(this));
       this._workspacesReorderedEvent = global.workspace_manager.connect('workspaces-reordered', this._onWorkspacesReordered.bind(this));
+      this._minimizedEvent = global.window_manager.connect('minimize', this._onWindowMinimized.bind(this));
     }
   
     _onWorkspaceAdded(_workspaceManager, workspaceIndex) {
-      let event = global.workspace_manager.get_workspace_by_index(workspaceIndex).connect('window-removed', this._onWindowRemoved.bind(this)); 
-      this._windowRemovedEvents.push(event);
+      let windowRemovedEvent = global.workspace_manager.get_workspace_by_index(workspaceIndex).connect('window-removed', this._onWindowRemoved.bind(this)); 
+      this._windowRemovedEvents.push(windowRemovedEvent);
     }
 
     _onWorkspaceRemoved(_workspaceManager, workspaceIndex) {
@@ -60,13 +61,17 @@ var AutoActivities = GObject.registerClass(
             secondWorkspaceIndex = i;
         }
 
-      let tempFirstWorkspaceIndex = this._windowRemovedEvents[firstWorkspaceIndex];
+      let tempFirstWorkspaceRemovedEvent = this._windowRemovedEvents[firstWorkspaceIndex];
       this._windowRemovedEvents[firstWorkspaceIndex] = this._windowRemovedEvents[secondWorkspaceIndex];
-      this._windowRemovedEvents[secondWorkspaceIndex] = tempFirstWorkspaceIndex;
+      this._windowRemovedEvents[secondWorkspaceIndex] = tempFirstWorkspaceRemovedEvent;
     }
 
-    _onWindowRemoved(_workspace, removedWindow) {
-      let ignoredWindowTypes = [ Meta.WindowType.DROPDOWN_MENU, Meta.WindowType.NOTIFICATION, Meta.WindowType.POPUP_MENU ];
+    _onWindowMinimized(_sender, actor) {
+      this._onWindowRemoved(_sender, actor.meta_window);
+    }
+
+    _onWindowRemoved(_sender, removedWindow) {
+      let ignoredWindowTypes = [ Meta.WindowType.DROPDOWN_MENU, Meta.WindowType.NOTIFICATION, Meta.WindowType.POPUP_MENU, Meta.WindowType.SPLASHSCREEN ];
       if (ignoredWindowTypes.includes(removedWindow.get_window_type()))
         return;
 
@@ -83,6 +88,8 @@ var AutoActivities = GObject.registerClass(
           windows = windows.filter(window => window.meta_window.get_monitor() === this._monitorIndex);
         if (this._settings.get_boolean('skip-taskbar'))
           windows = windows.filter(window => !window.meta_window.skip_taskbar);
+        if (this._settings.get_boolean('detect-minimized'))
+          windows = windows.filter(window => !window.meta_window.minimized);
 
         if (windows.length < 1)
           Main.overview.show();
@@ -93,6 +100,7 @@ var AutoActivities = GObject.registerClass(
       global.workspace_manager.disconnect(this._workspaceAddedEvent);
       global.workspace_manager.disconnect(this._workspaceRemovedEvent);
       global.workspace_manager.disconnect(this._workspacesReorderedEvent);
+      global.window_manager.disconnect(this._minimizedEvent);
 
       for (let i = 0; i < this._windowRemovedEvents.length; i++)
         if (GObject.signal_handler_is_connected(global.workspace_manager.get_workspace_by_index(i), this._windowRemovedEvents[i]))
